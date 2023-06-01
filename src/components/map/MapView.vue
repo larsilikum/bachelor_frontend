8112359df649d0ae0d6b079647ac8b0c16fccda7
Lars Hembacher <lars.hembacher@students.hs-mainz.de>
2023-06-01 19:26:07 +0200

    start map view again

--- src/components/map/MapView.vue
+++ src/components/map/MapView.vue
@@ -1,188 +1,121 @@
 <template>
-  <div ref="myDiagramRef" id="myDiagramDiv" style="width: 100%; height: 600px;"></div>
+  <div ref="map"></div>
 </template>
 
 <script setup>
-import go from 'gojs';
-import {onMounted, ref} from "vue";
-import {useItemStore} from "../../store/index.js";
-import {useRouter} from "vue-router";
+import { ref, onMounted } from 'vue'
+import * as d3 from 'd3'
+import { useRouter } from 'vue-router'
+import {useItemStore} from '../../store/index.js'
 
-const itemStore = useItemStore()
+const store = useItemStore()
 const router = useRouter()
+const map = ref(null);
+const elements = ref([])
+const dimensions = ref({
+  width: 1000,
+  height: 600
+})
 
-const myDiagramRef = ref(null);
 
-const highlightColor = '#ef3920'
+onMounted(async() => {
+  elements.value = await store.getItems
+  const svg = d3.select(map.value).append("svg")
+      .attr("width", dimensions.value.width)
+      .attr("height", dimensions.value.height)
+  console.log(elements.value)
 
-class WheelLayout extends go.CircularLayout {
-  // override makeNetwork to set the diameter of each node and ignore the TextBlock label
-  makeNetwork(coll) {
-    const net = super.makeNetwork(coll);
-    net.vertexes.each((cv) => (cv.diameter = 20)); // because our desiredSize for nodes is (20, 20)
-    return net;
-  }
+  // Create a map to hold counts of references for each element
+  const referenceCounts = new Map();
 
-  // override commitNodes to rotate nodes so the text goes away from the center,
-  // and flip text if it would be upside-down
-  commitNodes() {
-    super.commitNodes();
-    this.network.vertexes.each((v) => {
-      const node = v.node;
-      if (node === null) return;
-      // get the angle of the node towards the center, and rotate it accordingly
-      const a = v.actualAngle;
-      if (a > 90 && a < 270) {
-        // make sure the text isn't upside down
-        const textBlock = node.findObject('TEXTBLOCK');
-        textBlock.angle = 180;
+  elements.value.forEach(element => {
+    element.references.forEach(reference => {
+      const referencedId = reference.related_item_id.id;
+      if (referenceCounts.has(referencedId)) {
+        referenceCounts.set(referencedId, referenceCounts.get(referencedId) + 1);
+      } else {
+        referenceCounts.set(referencedId, 1);
       }
-      node.angle = a;
     });
-  }
-
-  // override commitLinks in order to make sure all of the Bezier links are "inside" the ellipse;
-  // this helps avoid links crossing over any other nodes
-  commitLinks() {
-    super.commitLinks();
-    if (this.network.vertexes.count > 4) {
-      this.network.vertexes.each((v) => {
-        v.destinationEdges.each((de) => {
-          const dv = de.toVertex;
-          let da = dv.actualAngle;
-          let sa = v.actualAngle;
-          if (da - sa > 180) da -= 360;
-          else if (sa - da > 180) sa -= 360;
-          de.link.curviness = sa > da ? 15 : -15;
-        });
-      });
-    }
-  }
-}
-
-onMounted(async () => {
-  const items = await itemStore.fetchItems()
-  console.log(items)
-
-  const myDiagram = go.GraphObject.make(go.Diagram, myDiagramRef.value, {
-    initialAutoScale: go.Diagram.Uniform,
-    padding: 50,
-    contentAlignment: go.Spot.Center,
-    layout: go.GraphObject.make(WheelLayout, {
-      arrangement: go.CircularLayout.ConstantDistance,
-      nodeDiameterFormula: go.CircularLayout.Circular,
-      spacing: 10,
-      aspectRatio: 1,
-      sorting: go.CircularLayout.Optimized
-    }),
-    isReadOnly: true,
-    click: (e) => {
-      e.diagram.startTransaction('clear');
-      e.diagram.clearHighlighteds();
-      e.diagram.commitTransaction('clear');
-    }
   });
 
-  myDiagram.nodeTemplate = go.GraphObject.make(go.Node, 'Horizontal', {
-        selectionAdorned: false,
-        locationSpot: go.Spot.Center,
-        locationObjectName: 'SHAPE',
-        mouseEnter: (e, node) => {
-          node.diagram.clearHighlighteds();
-          node.linksConnected.each((l) => highlightLink(l, true));
-          node.isHighlighted = true;
-          const tb = node.findObject('TEXTBLOCK');
-          if (tb !== null) {
-            tb.stroke = highlightColor;
-            tb.opacity = 1;
-          }
-        },
-        mouseLeave: (e, node) => {
-          node.diagram.clearHighlighteds();
-          const tb = node.findObject('TEXTBLOCK');
-          if (tb !== null) {
-            tb.stroke = 'transparent';
-            tb.opacity = 0;
-          }
-        },
-        click: (e, node) => {
-          router.push({name: 'Item', params: {id: node.data.key}})
-        }
-      }, new go.Binding('text', 'text'),
-      go.GraphObject.make(go.Shape, 'Ellipse', {
-            name: 'SHAPE',
-            fill: 'lightgray',
-            stroke: 'transparent',
-            strokeWidth: 2,
-            desiredSize: new go.Size(10, 10),
-            portId: '',
-          }, new go.Binding('fill', 'color'),
-          new go.Binding('stroke', 'isHighlighted', (h) => (h ? highlightColor : 'transparent')).ofObject(),
-          new go.Binding('desiredSize', 'references', (refs) => {
-            const size = calculateNodeSize(refs);
-            return new go.Size(size, size);
-          })),
-      go.GraphObject.make(go.TextBlock, {
-        name: 'TEXTBLOCK',
-        opacity: 0
-      }, new go.Binding('text', 'text')));
+  // line Dataset
+  let linesData = [];
+  elements.value.forEach(element => {
+    // Add logic if element is hovered
+    element.isHovered = false
 
-  function highlightLink(link, show) {
-    link.isHighlighted = show;
-    link.fromNode.isHighlighted = show;
-    link.toNode.isHighlighted = show;
-  }
+    // Add a referenceCount property to each element
+    element.referenceCount = referenceCounts.get(element.id) || 0;
 
-  myDiagram.linkTemplate = go.GraphObject.make(go.Link, {
-    routing: go.Link.Normal,
-    curve: go.Link.Bezier,
-    selectionAdorned: false,
-    mouseEnter: (e, link) => highlightLink(link, true),
-    mouseLeave: (e, link) => highlightLink(link, false)
-  }, go.GraphObject.make(go.Shape, new go.Binding('stroke', 'isHighlighted', (h, shape) => (h ? highlightColor : 'transparent')).ofObject(), new go.Binding('strokeWidth', 'isHighlighted', (h) => (h ? 2 : 1)).ofObject()));
+    // Add the radius size
+    element.radiusSize = Math.sqrt(element.referenceCount * 5000 + 400)
 
-
-  function generateGraph() {
-    const nodeDataArray = items.map((item) => ({
-      key: item.id,
-      text: item.title,
-      color: '#331917',
-      references: item.references ? item.references.length : 0
-    }));
-
-    const linkDataArray = [];
-    items.forEach((item) => {
-      item.references.forEach((reference) => {
-        linkDataArray.push({
-          from: item.id,
-          to: reference.related_item_id.id,
-          color: go.Brush.randomColor(0, 127)
-        });
-      });
+    element.references.forEach(reference => {
+      let relatedElement = elements.value.find(e => e.id === reference.related_item_id.id);
+      if (relatedElement) {
+        linesData.push({ source: element, target: relatedElement });
+      }
     });
+  });
 
-    myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-  }
+  elements.value.sort((a , b) => b.radiusSize - a.radiusSize)
 
-  function calculateNodeSize(numConnections) {
-    if (numConnections === 0) {
-      return 10;
-    } else if (numConnections === 1) {
-      return 11;
-    } else {
-      return Math.round(Math.pow(1.3, numConnections - 2) * 13);
-    }
-  }
 
-  generateGraph();
+  // Draw circles...
+  const circles = svg.selectAll("circle")
+      .data(elements.value)  // bind elements to circles
+      .enter().append("circle")  // create new circle for each element
+      .attr("r", d => d.radiusSize)  // radius depends on number of references
+      .attr("cx", d => {
+        d.x = Math.random() * dimensions.value.width
+        return d.x
+      })  // random x position
+      .attr("cy", d => {
+        d.y = Math.random() * dimensions.value.height
+        return d.y
+      }) // random y position
+      .style("fill", d => `rgb(${d.referenceCount * 40},${d.referenceCount * 40},${d.referenceCount * 40})`)
+
+  // Draw lines...
+  const lines = svg.selectAll("line")
+      .data(linesData)
+      .enter().append("line")
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y)
+      .style("stroke", "black")
+      .style("opacity", 0)
+
+  // Draw text
+  const texts = svg.selectAll("text")
+      .data(elements.value)
+      .enter().append("text")
+      .attr("x", d => d.x + d.radiusSize + 8)  // position text 8px to the right of the circle
+      .attr("y", d => d.y + 7)  // align the vertical position of the text with the circle
+      .text(d => d.title)  // set the text content to the element's title
+      .style("font-size", "20px")  // set the font size
+      .style("opacity", 0);  // hide the text initially
+
+
+  // Handle interactions...
+  circles
+      .on("mouseenter", (event, data) => {
+        // Handle mouse enter...
+        data.isHovered = true
+        lines.style("opacity", d => d.source.isHovered || d.target.isHovered ? 1 : 0)
+        texts.style("opacity", d => d.isHovered ? 1 : 0)
+      })
+      .on("mouseleave", (event, data) => {
+        // Handle mouse leave...
+        data.isHovered = false
+        lines.style("opacity", d => d.source.isHovered || d.target.isHovered ? 1 : 0)
+        texts.style("opacity", d => d.isHovered ? 1 : 0);
+      })
+      .on("click", (event, data) => {
+        router.push('/item/' + data.id)
+
+      });
 });
-
-
 </script>
-<style>
-#myDiagramDiv {
-  width: 100%;
-  height: calc(100vh - 100px);
-  border: 2px solid var(--pri);
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -9,24 +9,24 @@ export const useItemStore = defineStore('item', {
         items: []
     }),
     getters: {
-        getItems() {
+        async getItems() {
+            await this.fetchItems()
             return this.items
         },
         getItemById: (state) => {
-            return async(id) => {
-                const item = state.items.find(i => i.id === id)
+            return async id => {
+                const item = state.items.find(i => i.id == id, state)
                 if(item) return item
                 return await state.fetchItemById(id)
-
             }
         }
     },
     actions: {
         async fetchItems() {
             const response = await directus.items('item').readByQuery({
-                fields: ['*.*.*']
+                fields: ['*.*.*.*']
             })
-            return response.data
+            this.items = response.data
         },
         async fetchItemById(id) {
             const response = await directus.items('item').readByQuery({
@@ -37,7 +37,6 @@ export const useItemStore = defineStore('item', {
             })
             const item = response.data[0]
             this.items.push(item)
-            console.log(item)
             return item
         }
     }
